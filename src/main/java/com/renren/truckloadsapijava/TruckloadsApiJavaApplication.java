72ed5f51b5afc0069355210161580b3ca4260330
andrew <andrew@andrews-MacBook-Pro.local>
2018-05-07 16:40:15 +0800

    [cr-1]init

---
+++ src/main/java/com/renren/truckloadsapijava/TruckloadsApiJavaApplication.java
@@ -0,0 +1,12 @@
+package com.renren.truckloadsapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckloadsApiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckloadsApiJavaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/renren/truckloadsapijava/TruckloadsApiJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.renren.truckloadsapijava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TruckloadsApiJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
